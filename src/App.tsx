741eb0792420a54e35aac365f3f2ff15dc7258e9
Mahmoud Mohsen <mahmoud.mohsen.developer@gmail.com>
2025-08-18 01:59:10 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  // const title = "React";
+  return <div className="border-l text-3xl text-red-500">Hello world</div>;
+}
+
+export default App;
