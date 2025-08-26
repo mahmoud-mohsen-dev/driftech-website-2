45ee2cbe23eafd6e8a6d47e474d816c80afe9305
Mahmoud Mohsen <mahmoud.mohsen.developer@gmail.com>
2025-08-27 02:11:44 +0300

    moved browserRouter from main.tsx to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import Homepage from "./pages/HomePage";
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomePage from "./pages/Homepage";
+import AuthLayout from "./features/auth/AuthLayout";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
-  return <Homepage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+
+        <Route element={<AuthLayout />}>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<SignupPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
-// import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router";
-import HomePage from "./pages/HomePage.tsx";
-import AuthLayout from "./features/auth/AuthLayout.tsx";
-import LoginPage from "./pages/LoginPage.tsx";
-import SignupPage from "./pages/SignupPage.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<HomePage />} />
-
-        <Route element={<AuthLayout />}>
-          <Route path="login" element={<LoginPage />} />
-          <Route path="register" element={<SignupPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
